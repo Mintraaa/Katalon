2f341f8aa19bd3a7470fde37b4613368a525af57
Mintraaa <111554748+Mintraaa@users.noreply.github.com>
2024-01-16 16:18:59 +0700

    Add files via upload

---
+++ My First Web UI Project/Test Suites/Keyword All Flie By Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Keyword All Flie By Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>072102a7-edaa-4560-8536-a0c4f8c6a7d6</testSuiteGuid>
+</TestSuiteEntity>
